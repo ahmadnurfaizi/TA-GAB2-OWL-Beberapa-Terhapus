5ad8ec45c30e955bce94d12e371806c1ba1268b1
ahmadnurfaizi <ahmadnurfaizi357>
2023-01-07 17:08:37 +0700

    master

---
+++ kavlingFrontend/app/Http/Livewire/Folder/Nama.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Folder;
+
+use Livewire\Component;
+
+class Nama extends Component
+{
+    public function render()
+    {
+        return view('livewire.folder.nama');
+    }
+}
